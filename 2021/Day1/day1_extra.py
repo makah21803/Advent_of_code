91aebf0b7ba2c4ff429521b6227e9ba1c39e2698
makah21803 <makah21803@gmail.com>
2021-12-01 09:07:09 +0100

    improved and cleaned

--- 2021/Day1/day1_extra.py
+++ 2021/Day1/day1_extra.py
@@ -1,24 +1,22 @@
-import os
-import re
 
-dirname = os.path.dirname(__file__)
-file = open(dirname+"/input_data.txt", "r")
+file = open("input_data.txt", "r")
 
 list = file.readlines()
 int_list = [int(i) for i in list]
 
-increased_counter = 0
-previous_depths = [d for i, d in enumerate(int_list) if i < 3]
-for i, depth in enumerate(int_list):
-    if i == 0 or i > len(int_list)-3:
-        continue
-    previous_sum = sum([int_list[i+n-1] for n in range(3)])
-    current_sum = sum([int_list[i+n] for n in range(3)])
+counter = 0
+
+for i in range(1, len(int_list)-2):
+
+    # previous_sum = sum([int_list[i+n-1] for n in range(3)])
+    # current_sum = sum([int_list[i+n] for n in range(3)])
+    previous_sum = sum(int_list[i-1:i+2])
+    current_sum = sum(int_list[i:i+3])
 
     if current_sum > previous_sum:
-        increased_counter += 1
+        counter += 1
 
-    print("depth: {}\nprevious sum: {}\ncurrent sum: {}\n".format(depth, previous_sum, current_sum))
+    print("depth: {}\nprevious sum: {}\ncurrent sum: {}\n".format(int_list[i], previous_sum, current_sum))
 
-print("answer is: {}".format(increased_counter))
+print("answer is: {}".format(counter))
 
