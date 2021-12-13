2268aab51ff489f76bb59b03a0e24eee9f439f18
makah21803 <makah21803@gmail.com>
2021-12-13 11:29:08 +0100

    day3 comments

--- 2021/Day3/day3_extra.py
+++ 2021/Day3/day3_extra.py
@@ -8,33 +8,53 @@ list = file.readlines()
 class OxygenLevels():
     def __init__(self, list):
         self.bin_len = len(list[0])
-        self.oxy_list = [s.strip() for s in list]
-        self.coo_list = [s.strip() for s in list]
+        self.list = [s.strip() for s in list]
 
     def run(self):
-        oxy = self.generator(self.oxy_list)
-        coo = self.generator(self.oxy_list, invert=True)
+        oxy = self.generator(self.list)
+        coo = self.generator(self.list, invert=True)
         return [oxy, coo]
 
     def find_common(self, list, index):
+        """
+        Returns the most common bin value on the given index of list entries
+        :param list: [str]
+        :param index: int
+        :return: int
+        """
         cumulative = 0
         for line in list:
+            # adding each value in the given index: +1 or +0
             cumulative += int(line[index])
+        # true if 1 was the most common
         return int(cumulative >= len(list)/2)
 
     def generator(self, entries, invert=False):
+        """
+        Iterates through each 0|1 of the entries (binary length defined by self.bin_len)
+        :param entries: [str]
+        :param invert: bool
+        :return: str
+        """
+
         for n in range(self.bin_len):
             common = self.find_common(entries, n)
-            entries_tmp = [s for s in entries]
 
+            # necessary for modifying list while iterating through it
+            entries_tmp = [s for s in entries]
             for entry in entries:
+                # entry is binary string, i.e. '100000101101', int(entry[n]) is the n-th digit in that string
+                # comparing whether it is the common with True or False depending on Oxy or Coo
+                # Oxy (invert = False): not common is not False -> remove when not common
+                # Coo (invert = True ): not common is not True  -> keep when not common, i.e. remove when common
                 if (int(entry[n]) != common) is not invert:
                     entries_tmp.remove(entry)
             entries = entries_tmp
 
+            # last remaining, return the value
             if len(entries) == 1:
                 return entries[0]
-                break
+
         return False
 
 
